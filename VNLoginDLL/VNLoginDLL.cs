408129a242e83614a861333a2fc2a6dba5696b28
vodinhtu.com <107798779+dinhtu1100@users.noreply.github.com>
2022-07-28 14:30:00 +0700

    Add project files.

---
+++ VNLoginDLL/VNLoginDLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNLoginDLL
+{
+    public class VNLoginDLL
+    {
+        [DllImport("VNLogin.Library.dll", EntryPoint = "CreateWin", CallingConvention = CallingConvention.StdCall)]
+        public static extern void CreateWin(string uid, string nameProfile, string fullproxy);
+
+        [DllImport("VNLogin.Library.dll", EntryPoint = "CreateMac", CallingConvention = CallingConvention.StdCall)]
+        public static extern void CreateMac(string uid, string nameProfile, string fullproxy);
+
+        [DllImport("VNLogin.Library.dll", EntryPoint = "CreateLinux", CallingConvention = CallingConvention.StdCall)]
+        public static extern void CreateLinux(string uid, string nameProfile, string fullproxy);
+
+        [DllImport("VNLogin.Library.dll", EntryPoint = "CreateAndroid", CallingConvention = CallingConvention.StdCall)]
+        public static extern void CreateAndroid(string uid, string nameProfile, string fullproxy);
+
+        [DllImport("VNLogin.Library.dll", EntryPoint = "CreateIphone", CallingConvention = CallingConvention.StdCall)]
+        public static extern void CreateIphone(string uid, string nameProfile, string fullproxy);
+
+        [DllImport("VNLogin.Library.dll", EntryPoint = "SetProxy", CallingConvention = CallingConvention.StdCall)]
+        public static extern void SetProxy(string id, string fullproxy);
+
+        [DllImport("VNLogin.Library.dll", EntryPoint = "Backup", CallingConvention = CallingConvention.StdCall)]
+        public static extern void Backup(string id, string pathBackup);
+    }
+}
---
+++ VNLoginDLL/frmMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VNLoginDLL
+{
+    public partial class frmMain : Form
+    {
+        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnwin_Click(object sender, EventArgs e)
+        {
+            VNLoginDLL.CreateWin(txtid1.Text, txtname.Text, txtproxy1.Text);
+        }
+
+        private void btnmac_Click(object sender, EventArgs e)
+        {
+            VNLoginDLL.CreateMac(txtid1.Text, txtname.Text, txtproxy1.Text);
+        }
+
+        private void btnlinux_Click(object sender, EventArgs e)
+        {
+            VNLoginDLL.CreateLinux(txtid1.Text, txtname.Text, txtproxy1.Text);
+        }
+
+        private void btnandroid_Click(object sender, EventArgs e)
+        {
+            VNLoginDLL.CreateAndroid(txtid1.Text, txtname.Text, txtproxy1.Text);
+        }
+
+        private void btniphone_Click(object sender, EventArgs e)
+        {
+            VNLoginDLL.CreateIphone(txtid1.Text, txtname.Text, txtproxy1.Text);
+        }
+
+        private void btnsetproxy_Click(object sender, EventArgs e)
+        {
+            VNLoginDLL.SetProxy(txtid2.Text, txtproxy2.Text);
+        }
+
+        private void btnbackup_Click(object sender, EventArgs e)
+        {
+            VNLoginDLL.Backup(txtid3.Text, txtbackup.Text);
+        }
+    }
+}
