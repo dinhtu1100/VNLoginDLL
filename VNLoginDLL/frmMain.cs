a795ac4ec10ca5d152fe7b5c04d399e6ca027cae
vodinhtu.com <107798779+dinhtu1100@users.noreply.github.com>
2022-08-14 10:54:04 +0700

    t

--- VNLoginDLL/frmMain.cs
+++ VNLoginDLL/frmMain.cs
@@ -21,7 +21,13 @@ namespace VNLoginDLL
             InitializeComponent();
         }
 
-        
+        string proxytype = "no";
+        string ip = "";
+        string port = "";
+        string userproxy = "";
+        string passproxy = "";
+        string Orbitar = Application.StartupPath + "\\data\\orbita-browser\\";
+
         private void btnwin_Click(object sender, EventArgs e)
         {
             //user license
@@ -33,12 +39,6 @@ namespace VNLoginDLL
             //userip
             //passip
             //trang web mở cùng profile
-            string proxytype = "no";
-            string ip = "";
-            string port = "";
-            string userproxy = "";
-            string passproxy = "";
-            string Orbitar = Application.StartupPath + "\\data\\orbita-browser\\";
             string id = VNLoginDLL.Win("vnlogin", "vnlogin", "Win", proxytype, ip, port, userproxy, passproxy, "https://vnlogin.com");
             MessageBox.Show("Tạo thành công Profile có ID: " + id);
 
@@ -51,24 +51,40 @@ namespace VNLoginDLL
         {
             string id = VNLoginDLL.Mac("vnlogin", "vnlogin", "Mac", "no", "", "", "", "", "https://vnlogin.com");
             MessageBox.Show("Tạo thành công Profile có ID: " + id);
+
+            //sau khi tạo mở profile và trả về PID để kill process
+            string PID = VNLoginDLL.Open(Orbitar, id, proxytype, ip, port);
+            MessageBox.Show("Mở thành công Profile có PID: " + PID);
         }
 
         private void btnlinux_Click(object sender, EventArgs e)
         {
             string id = VNLoginDLL.Linux("vnlogin", "vnlogin", "Linux", "no", "", "", "", "", "https://vnlogin.com");
             MessageBox.Show("Tạo thành công Profile có ID: " + id);
+
+            //sau khi tạo mở profile và trả về PID để kill process
+            string PID = VNLoginDLL.Open(Orbitar, id, proxytype, ip, port);
+            MessageBox.Show("Mở thành công Profile có PID: " + PID);
         }
 
         private void btnandroid_Click(object sender, EventArgs e)
         {
             string id = VNLoginDLL.Android("vnlogin", "vnlogin", "Android", "no", "", "", "", "", "https://vnlogin.com");
             MessageBox.Show("Tạo thành công Profile có ID: " + id);
+
+            //sau khi tạo mở profile và trả về PID để kill process
+            string PID = VNLoginDLL.Open(Orbitar, id, proxytype, ip, port);
+            MessageBox.Show("Mở thành công Profile có PID: " + PID);
         }
 
         private void btniphone_Click(object sender, EventArgs e)
         {
             string id = VNLoginDLL.Iphone("vnlogin", "vnlogin", "Iphone", "no", "", "", "", "", "https://vnlogin.com");
             MessageBox.Show("Tạo thành công Profile có ID: " + id);
+
+            //sau khi tạo mở profile và trả về PID để kill process
+            string PID = VNLoginDLL.Open(Orbitar, id, proxytype, ip, port);
+            MessageBox.Show("Mở thành công Profile có PID: " + PID);
         }
 
 
